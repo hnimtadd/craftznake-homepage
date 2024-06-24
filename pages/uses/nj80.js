5609bbd377fda06b1c82be6cde22a2aea54fea19
Dat Nguyen <minhdat15012002@gmail.com>
2024-06-25 01:03:24 +0700

    feat: add uses pages

---
+++ pages/uses/nj80.js
@@ -0,0 +1,33 @@
+import Layout from '@/components/layouts/article'
+import Paragraph from '@/components/paragraph'
+import { Meta, Title } from '@/components/title'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Badge, Link, List, ListItem, Text } from '@chakra-ui/react'
+
+//TODO: add image list or caseroul
+const UseKeyboardNJ = () => {
+  return (
+    <Layout title={'NJ80'}>
+      <Title content={'uses'}>
+        NJ80 <Badge>10/2022</Badge>
+      </Title>
+      <Paragraph>
+        I use the Keydous NJ80 AP mechanical keyboard to boost my productivity
+        with its compact layout, durable PBT keycaps, and tactile Kailh Box
+        Brown Switches. The vibrant RGB LED lighting adds a touch of style to my
+        workspace, making it a valuable addition to my setup.
+      </Paragraph>
+      <List ml={4} mb={4}>
+        <ListItem>
+          <Meta>Link</Meta>
+          <Link href="https://www.amazon.com/keydous-Wireless-Mechanical-Keyboard-Bluetooth/dp/B09WTYMBT8">
+            Amazon <ExternalLinkIcon />
+          </Link>
+        </ListItem>
+        //TODO: add new list item here
+        <ListItem></ListItem>
+      </List>
+    </Layout>
+  )
+}
+export default UseKeyboardNJ
