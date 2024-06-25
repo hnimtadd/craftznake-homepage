3399b3aa1d446e3f9b8bff74a7e4ac40fa7c703c
Dat Nguyen <minhdat15012002@gmail.com>
2024-06-26 01:22:39 +0700

    feat: update works page

--- pages/works.js
+++ pages/works.js
@@ -2,18 +2,61 @@ import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '@/components/section'
 import { WorkGridItem } from '@/components/grid-item'
 import Layout from '@/components/layouts/article'
-import thumbSendit from '@/public/images/thumbnail.jpg'
-
-import thumbGoTodo from '@/public/images/thumbnail.jpg'
+import thumbSendit from '@/public/images/sendit/Sendit.png'
+import thumbGoTodo from '@/public/images/todo/todo.png'
+import thumbSmartHome from '@/public/images/smarthome/smarthome.png'
+import thumbUWC from '@/public/images/uwc/uwc.png'
+import thumbBlocker from '@/public/images/blocker/blocker.png'
+import thumbRun from '@/public/images/run/run.png'
+import thumbBlinders from '@/public/images/blinders/blinders.png'
+import thumbPeakee from '@/public/images/peakee/peakee.png'
 
 const Works = () => {
   return (
     <Layout title={'Works'}>
       <Container>
         <Heading as="h3" fontSize={20} mb="4">
-          Works
+          Zenonian Labs
         </Heading>
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem id="peakee" title="Peakee" thumbnail={thumbPeakee}>
+              Language learning platform that enable users learn english in
+              invisible way.
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem
+              id="blinder"
+              title="Blinder"
+              thumbnail={thumbBlinders}
+            >
+              Monorepo, serverless, microservices backend for a language
+              learning platform - Peakee.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+
+        <Section delay={0.2}>
+          <Divider my={6} />
+        </Section>
+
+        <Heading as="h3" fontSize={20} mb="4">
+          Side Projects
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem id="run" title="Run" thumbnail={thumbRun}>
+              Create, Deploy and Run you code from anywhere with a few-step
+              without cold-start.
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem id="blocker" title="Blocker" thumbnail={thumbBlocker}>
+              Blockchain network build from scratch, user could create their own
+              wallet and share data across the network.
+            </WorkGridItem>
+          </Section>
           <Section>
             <WorkGridItem
               id="senditsh"
@@ -36,14 +79,14 @@ const Works = () => {
 
         <Section delay={0.2}>
           <Divider my={6} />
-
-          <Heading as="h3" fontSize={20} mb={4}>
-            Collaborations
-          </Heading>
         </Section>
+
+        <Heading as="h3" fontSize={20} mb={4}>
+          Collaborations
+        </Heading>
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
           <Section>
-            <WorkGridItem id="uwc" title="UWC-Web" thumbnail={thumbSendit}>
+            <WorkGridItem id="uwc" title="UWC" thumbnail={thumbUWC}>
               Urban garbage collect management application on web platform.
             </WorkGridItem>
           </Section>
@@ -51,7 +94,7 @@ const Works = () => {
             <WorkGridItem
               id="smarthome"
               title="IOT Smarthome"
-              thumbnail={thumbSendit}
+              thumbnail={thumbSmartHome}
             >
               The mobile application that enable user to manage iot device in
               their house.
